61f00b678a663efc78db8e54df7e3ea0ac90ecd7
ryanbourdais <ryanbourdais@outlook.com>
2023-03-24 10:49:56 -0500

    added notes and reorged code

--- main.go
+++ main.go
@@ -4,36 +4,52 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/go-git/go-git/v5"
 	. "github.com/go-git/go-git/v5/_examples"
 )
 
 func main() {
-	command := "sh"
-	script := "./test.sh"
-	image := "java17_test:edge"
+	for {
+		CheckArgs("<path>")
+		path := os.Args[1]
 
-	cmd := exec.Command(command, script, image)
-	stdout, err := cmd.Output()
+		// We instantiate a new repository targeting the given path (the .git folder)
+		r, err := git.PlainOpen(path)
+		CheckIfError(err)
 
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+		// Get the working directory for the repository
+		w, err := r.Worktree()
+		CheckIfError(err)
+
+		status := w.Pull(&git.PullOptions{})
+		currentStatus := status.Error()
+
+		if currentStatus == "already up-to-date" {
+			fmt.Println("no changes")
+		} else {
+			command := "sh"
+			script := "./test.sh"
+			image := "java17_test:edge"
+
+			cmd := exec.Command(command, script, image)
+			stdout, err := cmd.Output()
+
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			fmt.Print(string(stdout))
+		}
+		time.Sleep(10 * time.Second)
 	}
-
-	fmt.Print(string(stdout))
-
-	CheckArgs("<path>")
-	path := os.Args[1]
-
-	// We instantiate a new repository targeting the given path (the .git folder)
-	r, err := git.PlainOpen(path)
-	CheckIfError(err)
-
-	// Get the working directory for the repository
-	w, err := r.Worktree()
-	CheckIfError(err)
-
-	fmt.Print(w.Pull(&git.PullOptions{}))
 }
+
+//FIND A WAY TO CHECK EVERY 30 SECONDS FOR FOR LOOP OF ALL REPOS
+//run this script in a base docker image to fire off new docker images with the proper image
+//have a db that has a bool for "changed" this script changes that value and fires off the corresponding image with the env var of the repo link then changes "changed" to false
+//image runs CI.sh and outputs a directory
+
+//hook all of this up to a frontend
